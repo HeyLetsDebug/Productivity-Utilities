5c990db77090722f03ba72cd3ec8536ffc688d56
Abhishek Dubey <38434912+HeyLetsDebug@users.noreply.github.com>
2021-07-31 18:38:49 +0530

    Image Compressor Updated Added Image compressor and working on improving its functionality

--- src/pages/ImageCompressor.js
+++ src/pages/ImageCompressor.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { Component } from "react";
+import { useState } from "react";
 import { Row, Col, Card, Container } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import imageCompression from "browser-image-compression";
-import uploadPlaceholder from "../img/file-upload-placeholder.png";
-import placeHolderImage from "../img/video-poster-placeholder.jpg";
 
 export default function ImageOptimizer() {
-  const [compressedLink, setCompressedLink] = useState(placeHolderImage);
+  const [compressedLink, setCompressedLink] = useState(
+    "https://testersdock.com/wp-content/uploads/2017/09/file-upload-1280x640.png"
+  );
   const [originalImage, setOriginalImage] = useState("");
   const [originalLink, setOriginalLink] = useState("");
   const [clicked, setClicked] = useState(false);
@@ -16,11 +16,15 @@ export default function ImageOptimizer() {
 
   function handle(e) {
     const imageFile = e.target.files[0];
-    setOriginalImage(imageFile);
-    setOriginalLink(URL.createObjectURL(imageFile));
-    setOutputFileName((outputFileName) => imageFile.name);
+    setOriginalImage(URL.createObjectURL(imageFile));
+    setOriginalLink(imageFile);
+    setOutputFileName(imageFile.name);
     setUploadImage(true);
   }
+
+  // function changeValue (e) {
+  //   this.setState({ [e.target.name]: e.target.value });
+  // };
 
   function click(e) {
     e.preventDefault();
@@ -40,8 +44,11 @@ export default function ImageOptimizer() {
       output = x;
 
       const downloadLink = URL.createObjectURL(output);
+      // this.setState({
+      //   compressedLink: downloadLink
+      // });
 
-      setCompressedLink((compressedLink) => downloadLink);
+      setCompressedLink(downloadLink);
     });
 
     setClicked(true);
@@ -50,71 +57,64 @@ export default function ImageOptimizer() {
 
   return (
     <>
-      <Container className="pt-5">
-        <div>
-          <div className="text-center">
-            <h1>React Image Compressor</h1>
+      <div className="m-5">
+        <div className="text-light text-center">
+          <h1>React Image Compressor</h1>
+        </div>
+
+        <div className="row mt-5">
+          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
+            {uploadImage ? (
+              <Card.Img
+                className="ht"
+                variant="top"
+                src={originalLink}
+              ></Card.Img>
+            ) : (
+              <Card.Img
+                className="ht"
+                variant="top"
+                src="https://testersdock.com/wp-content/uploads/2017/09/file-upload-1280x640.png"
+              ></Card.Img>
+            )}
+            <div className="d-flex justify-content-center">
+              <input
+                type="file"
+                accept="image/*"
+                className="mt-2 btn btn-dark w-75"
+                onChange={handle}
+              />
+            </div>
+          </div>
+          <div className="col-xl-4 col-lg-4 col-md-12 mb-5 mt-5 col-sm-12 d-flex justify-content-center align-items-baseline">
+            <br />
+            {outputFileName ? (
+              <button type="button" className=" btn btn-dark" onClick={click}>
+                Compress
+              </button>
+            ) : (
+              <></>
+            )}
           </div>
 
-          <Row className="mt-5">
-            <Col xl="4" lg="4" md="12" sm="12">
-              {uploadImage ? (
-                <Card.Img
-                  className="ht"
-                  variant="top"
-                  src={originalLink}
-                ></Card.Img>
-              ) : (
-                <Card.Img
-                  className="ht"
-                  variant="top"
-                  src={uploadPlaceholder}
-                ></Card.Img>
-              )}
+          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12 mt-3">
+            <Card.Img variant="top" src={compressedLink}></Card.Img>
+            {clicked ? (
               <div className="d-flex justify-content-center">
-                <input
-                  type="file"
-                  accept="image/*"
-                  className="mt-2 btn btn-dark"
-                  onChange={handle}
-                />
+                <a
+                  href={setCompressedLink}
+                  download={outputFileName}
+                  className="mt-2 btn btn-dark w-75"
+                >
+                  Download
+                </a>
               </div>
-            </Col>
-            <Col
-              xl="4"
-              lg="4"
-              md="12"
-              sm="12"
-              className="mt-5 mb-5 d-flex justify-content-center align-items-baseline"
-            >
-              {outputFileName ? (
-                <button type="button" className=" btn btn-dark" onClick={click}>
-                  Compress
-                </button>
-              ) : (
-                <></>
-              )}
-            </Col>
-
-            <Col xl="4" lg="4" md="12" sm="12" className="mt-3">
-              <Card.Img variant="top" src={compressedLink}></Card.Img>
-              {clicked ? (
-                <div className="d-flex justify-content-center">
-                  <a
-                    href={compressedLink}
-                    download={outputFileName}
-                    className="mt-2 btn btn-dark w-75"
-                  >
-                    Download
-                  </a>
-                </div>
-              ) : (
-                <></>
-              )}
-            </Col>
-          </Row>
+            ) : (
+              <></>
+            )}
+          </div>
         </div>
-      </Container>
+      </div>
 
       <Container className="pt-5 pb-5">
         <Row xs={1} md={4} className="g-4">
--- src/pages/ImageCropper.js
+++ src/pages/ImageCropper.js
@@ -1,108 +1,109 @@
-import React, { Component } from "react";
+import "../styles/cropper.css";
+import React from "react";
+import { useState, useRef } from "react";
 import { Row, Col, Card, Container } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
-export default class ImageCropper extends Component {
-  render() {
-    return (
-      <>
-        <Container className="pt-5 pb-5">
-          <Row xs={1} md={4} className="g-4">
-            <Col>
-              <Card>
+export default function ImageCropper() {
+  return (
+    <>
+      {/* cropper body section ends here */}
+      <Container className="pt-5 pb-5">
+        <Row xs={1} md={4} className="g-4">
+          <Col>
+            <Card>
+              <LinkContainer to="/pdf-merger">
+                <Card.Img
+                  variant="top"
+                  src="https://via.placeholder.com/180x90"
+                />
+              </LinkContainer>
+              <Card.Body>
                 <LinkContainer to="/pdf-merger">
-                  <Card.Img
-                    variant="top"
-                    src="https://via.placeholder.com/180x90"
-                  />
+                  <Card.Title>PDF Merge</Card.Title>
                 </LinkContainer>
-                <Card.Body>
-                  <LinkContainer to="/pdf-merger">
-                    <Card.Title>PDF Merge</Card.Title>
-                  </LinkContainer>
-                  <Card.Text>
-                    This is a longer card with supporting text below as a
-                    natural lead-in to additional content. This content is a
-                    little bit longer.
-                  </Card.Text>
-                  <LinkContainer to="/pdf-merger">
-                    <Card.Link>Visit</Card.Link>
-                  </LinkContainer>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col>
-              <Card>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+                <LinkContainer to="/pdf-merger">
+                  <Card.Link>Visit</Card.Link>
+                </LinkContainer>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <LinkContainer to="/pdf-meta-editor">
+                <Card.Img
+                  variant="top"
+                  src="https://via.placeholder.com/180x90"
+                />
+              </LinkContainer>
+              <Card.Body>
                 <LinkContainer to="/pdf-meta-editor">
-                  <Card.Img
-                    variant="top"
-                    src="https://via.placeholder.com/180x90"
-                  />
+                  <Card.Title>PDF Meta Editor</Card.Title>
                 </LinkContainer>
-                <Card.Body>
-                  <LinkContainer to="/pdf-meta-editor">
-                    <Card.Title>PDF Meta Editor</Card.Title>
-                  </LinkContainer>
-                  <Card.Text>
-                    This is a longer card with supporting text below as a
-                    natural lead-in to additional content. This content is a
-                    little bit longer.
-                  </Card.Text>
-                  <LinkContainer to="/pdf-meta-editor">
-                    <Card.Link>Visit</Card.Link>
-                  </LinkContainer>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col>
-              <Card>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+                <LinkContainer to="/pdf-meta-editor">
+                  <Card.Link>Visit</Card.Link>
+                </LinkContainer>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <LinkContainer to="/image-optimizer">
+                <Card.Img
+                  variant="top"
+                  src="https://via.placeholder.com/180x90"
+                />
+              </LinkContainer>
+              <Card.Body>
                 <LinkContainer to="/image-optimizer">
-                  <Card.Img
-                    variant="top"
-                    src="https://via.placeholder.com/180x90"
-                  />
+                  <Card.Title>Image Optimizer</Card.Title>
                 </LinkContainer>
-                <Card.Body>
-                  <LinkContainer to="/image-optimizer">
-                    <Card.Title>Image Optimizer</Card.Title>
-                  </LinkContainer>
-                  <Card.Text>
-                    This is a longer card with supporting text below as a
-                    natural lead-in to additional content. This content is a
-                    little bit longer.
-                  </Card.Text>
-                  <LinkContainer to="/image-optimizer">
-                    <Card.Link>Visit</Card.Link>
-                  </LinkContainer>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col>
-              <Card>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+                <LinkContainer to="/image-optimizer">
+                  <Card.Link>Visit</Card.Link>
+                </LinkContainer>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <LinkContainer to="/video-poster">
+                <Card.Img
+                  variant="top"
+                  src="https://via.placeholder.com/180x90"
+                />
+              </LinkContainer>
+              <Card.Body>
                 <LinkContainer to="/video-poster">
-                  <Card.Img
-                    variant="top"
-                    src="https://via.placeholder.com/180x90"
-                  />
+                  <Card.Title>Video Poster</Card.Title>
                 </LinkContainer>
-                <Card.Body>
-                  <LinkContainer to="/video-poster">
-                    <Card.Title>Video Poster</Card.Title>
-                  </LinkContainer>
-                  <Card.Text>
-                    This is a longer card with supporting text below as a
-                    natural lead-in to additional content. This content is a
-                    little bit longer.
-                  </Card.Text>
-                  <LinkContainer to="/video-poster">
-                    <Card.Link>Visit</Card.Link>
-                  </LinkContainer>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </>
-    );
-  }
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+                <LinkContainer to="/video-poster">
+                  <Card.Link>Visit</Card.Link>
+                </LinkContainer>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
