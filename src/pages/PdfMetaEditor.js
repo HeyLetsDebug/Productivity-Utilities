1298e8d6ca6a8715dee31e69b08ee9305ae9cb0f
Abhishek Dubey <38434912+HeyLetsDebug@users.noreply.github.com>
2021-08-01 22:27:24 +0530

    PDF Meta Data Updated PDF Meta data editor updated with modify and download. Working on remove meta data and other UI aspects

--- src/pages/PdfMetaEditor.js
+++ src/pages/PdfMetaEditor.js
@@ -1,48 +1,92 @@
+import "../styles/pdfMetaEdit.css";
 import React, { Component } from "react";
 import { useState, useEffect } from "react";
 import { Row, Col, Card, Container, Button } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
-import { PDFDocument } from "pdf-lib";
-// import { Document, Page, pdfjs } from "react-pdf";
-import placeHolderImage from "../img/video-poster-placeholder.jpg";
+import { mergeIntoTypedArray, PDFDocument } from "pdf-lib";
+import { Document, Page, pdfjs } from "react-pdf";
+import dummyPDF from "../img/dummy-pdf-placeholder.pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 export default function PdfMetaEditor() {
-  const [pdfObjectUrl, setPdfObjectUrl] = useState("");
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
+  const [disable, setDisable] = useState(false);
+  const [pdfObjectUrl, setPdfObjectUrl] = useState(dummyPDF);
+  const [nameOfFile, setNameOfFile] = useState("");
+  const [titleInput, setTitleInput] = useState("");
+  const [addtitleInput, setAddtitleInput] = useState("No Title Found");
+
+  const [pageNumber, setPageNumber] = useState(1); //setting 1 to show fisrt page
 
   function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
+    setPageNumber(1);
   }
-  const cursorPointer = {
-    cursor: "pointer"
-  };
 
   function handlePDFChange(e) {
     const pdfFile = e.target.files[0];
 
-    const mime_types = ["application/pdf"];
-
-    if (mime_types.indexOf(pdfFile.type) === -1) {
-      alert("Error : Incorrect file type");
-      return;
-    }
-
     setPdfObjectUrl((pdfObjectUrl) => URL.createObjectURL(pdfFile));
 
-    // console.log(e.target.files.files.length);
-
-    // for (var i = 0; i < e.target.files.length; i++) {
-    //   listofFiles = [];
-    //   nameOfFile = [];
-    //   nameOfFile = e.target.files[i].name;
-    //   listofFiles.push(URL.createObjectURL(e.target.files[i]));
-    // }
+    setNameOfFile((nameOfFile) => e.target.files[0].name);
   }
 
   useEffect(() => {
-    console.log(pdfObjectUrl);
+    readDocumentMetadata(pdfObjectUrl);
+    setDisable(false);
   }, [pdfObjectUrl]);
+
+  useEffect(() => {
+    console.log(addtitleInput);
+  }, [addtitleInput]);
+
+  async function readDocumentMetadata(url) {
+    //const mergedPdf = await PDFDocument.create();
+    const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
+    var bytes = new Uint8Array(existingPdfBytes);
+    const pdfDoc = await PDFDocument.load(bytes, {
+      updateMetadata: false
+    });
+
+    // setAddtitleInput((addtitleInput) => pdfDoc.getTitle());
+
+    if (pdfDoc.getTitle() !== undefined) {
+      setAddtitleInput((addtitleInput) => pdfDoc.getTitle());
+    } else {
+      setAddtitleInput((addtitleInput) => "No Title Found");
+    }
+  }
+
+  function handleModify() {
+    editDocumentMetadata(pdfObjectUrl);
+  }
+
+  async function editDocumentMetadata(MeditPdf) {
+    // console.log(MeditPdf);
+    //const pdfDoc = await PDFDocument.create();
+    const existingPdfBytes = await fetch(MeditPdf).then((res) =>
+      res.arrayBuffer()
+    );
+    // var bytes = new Uint8Array(existingPdfBytes);
+    console.log(existingPdfBytes);
+
+    const pdfDoc = await PDFDocument.load(existingPdfBytes);
+    pdfDoc.setTitle(titleInput);
+    pdfDoc.setProducer("");
+    pdfDoc.setCreator("");
+
+    const pdfBytes = await pdfDoc.save();
+
+    console.log(pdfBytes);
+    var blob = new Blob([pdfBytes], { type: "application/pdf" });
+    var link = document.createElement("a");
+    link.href = window.URL.createObjectURL(blob);
+    link.download = "myFileName.pdf";
+    link.click();
+    //downloadFile(new Blob(pdfBytes, {type: "application/pdf"}), nameOfFile);
+
+    //download(pdfBytes, nameOfFile, "application/pdf");
+    //saveAs(pdfBytes, nameOfFile, {type: "application/pdf"});
+  }
+
   return (
     <>
       <Container id="meta-editor" className="pt-5">
@@ -50,17 +94,23 @@ export default function PdfMetaEditor() {
           <h1>PDF Meta Data Editor</h1>
         </div>
         <Row>
-          <Col>
-            {/* <Document file={pdfObjectUrl} onLoadSuccess={onDocumentLoadSuccess}>
-              <Page pageNumber={pageNumber} />
+          <Col className="d-flex justify-content-center">
+            <Document
+              file={pdfObjectUrl}
+              options={{ workerSrc: "/pdf.worker.js" }}
+              onLoadSuccess={onDocumentLoadSuccess}
+            >
+              <Page
+                pageNumber={pageNumber}
+                scale={0.3}
+                renderAnnotationLayer={false}
+              />
             </Document>
-            <p>
-              Page {pageNumber} of {numPages}
-            </p> */}
           </Col>
           <Col>
+            <p>Current PDF Title : {addtitleInput}</p>
             <input
-              className="change-title-input"
+              className="change-title-input mt-2 btn btn-dark w-100"
               type="file"
               accept="application/pdf"
               id="file-here"
@@ -72,14 +122,20 @@ export default function PdfMetaEditor() {
               className="change-title-input"
               type="text"
               id="title-of-pdf"
+              value={titleInput}
+              onChange={(e) => setTitleInput(e.target.value)}
               placeholder="Wait till title of PDF shows here..."
             />
             <div className="modify-button-wrapper">
-              <Button id="modify-button" disabled>
-                Modify & Save Title
+              <Button
+                id="modify-button"
+                disabled={disable}
+                onClick={handleModify}
+              >
+                Modify & Download
               </Button>
-              <Button id="remove-meta-button" disabled>
-                Remove Title
+              <Button id="remove-meta-button" disabled={disable}>
+                Remove Title & Download
               </Button>
             </div>
           </Col>
